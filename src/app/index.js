93de36deb044079cb1ff05c9653c0124f8d1ab36
cekrem <christianekrem@gmail.com>
2015-05-26 15:43:43 +0200

    basic styling skeleton

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import MainCtrl from './main/main.controller';
+
+angular.module('um', ['ngRoute', 'ngMaterial'])
+  .controller('MainCtrl', MainCtrl)
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
