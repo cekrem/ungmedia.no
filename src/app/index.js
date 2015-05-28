2ccb54bb501e6e2ac9ae3d64a48b45a810b9f012
cekrem <christianekrem@gmail.com>
2015-05-28 17:14:26 +0200

    alpha 2 (dummy content)

--- src/app/index.js
+++ src/app/index.js
@@ -11,6 +11,18 @@ angular.module('um', ['ngRoute', 'ngMaterial'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl as main'
       })
+      .when('/hvem', {
+        templateUrl: 'app/main/hvem.html'
+      })
+      .when('/hva', {
+        templateUrl: 'app/main/hva.html'
+      })
+      .when('/hvor', {
+        templateUrl: 'app/main/hvor.html'
+      })
+      .when('/hvordan', {
+        templateUrl: 'app/main/hvordan.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
